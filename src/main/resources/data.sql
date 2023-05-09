908b5eba76e38232900b104302553ee48cbbfc72
abhik540 <to.abhik.maharjan@gmail.com>
2023-05-09 17:20:06 -0500

    Update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO system_properties (system_key, system_value) VALUES ('scheduler.feeder.cron', '0 0 7,15,23 * * *');
+INSERT INTO system_properties (system_key, system_value) VALUES ('scheduler.feeder.cron', '0 0 8,20 * * *');
 INSERT INTO system_properties (system_key, system_value) VALUES ('scheduler.light.cron', '0 0 6,20 * * *');
 INSERT INTO system_properties (system_key, system_value) VALUES ('scheduler.email.cron', '0 0 0 9,10,11,12 5,6,7,8 ?');
 INSERT INTO system_properties (system_key, system_value) VALUES ('email.to', 'thesujan540@gmail.com,sujan.maharjan.2015@gmail.com');
-INSERT INTO system_properties (system_key, system_value) VALUES ('message', 'Test Future Email Ignore it');
+INSERT INTO system_properties (system_key, system_value) VALUES ('message', 'Suman, \\n mero sabai account details haru maile mero laptop ma rakheko chu..\\n find a document `personal_info.txt` file in desktop \\n username: sujan540 \\n password: sujan@123 \\n maile key mero sathi manoj lai deko chu his number is +1319-217-6156 \\n garage code: 7426');
 INSERT INTO system_properties (system_key, system_value) VALUES ('subject', 'Test Email');
 INSERT INTO system_properties (system_key, system_value) VALUES ('scheduler.feeder', 'TRUE');
